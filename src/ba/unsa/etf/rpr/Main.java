6c536378b811e66aa07833d4e1645e8bf0cf3b15
akurtovic4 <akurtovic4@etf.unsa.ba>
2021-10-22 13:33:40 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Unesi broj n: ");
+        Scanner ulaz = new Scanner(System.in);
+        int n = ulaz.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if (i % sumaCifara(i) == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static int sumaCifara(int n) {
+        int s = 0;
+        while (n != 0) {
+
+            int cifra = n % 10;
+            s = s + cifra;
+            n = n / 10;
+        }
+        return s;
+
+    }
+}
